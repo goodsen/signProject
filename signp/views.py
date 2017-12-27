9153d116cce47a19afaebae2ff70d26d68bc8d4c
batmangoodsen <1281198374@qq.com>
2017-12-27 09:11:24 +0800

    更改index.html样式

--- signp/views.py
+++ signp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
+
 
 def index(request):
     #return HttpResponse("Hello Django!")
     return render(request,"index.html")
-# Create your views here.
+#登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if username == 'admin' and password == 'admin123':
+            return HttpResponse('登录成功')
+        else:
+            return render(request,'index.html'),{'error':'username or password error!'}
